fba206db746139f08e595c330bc940d383f934dd
sanvi122008 <76726412+sanvi122008@users.noreply.github.com>
2021-12-15 16:07:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+noseX=0;
+noseY=0;
+
+function preload()
+{
+clown_nose = loadImage('https://i.postimg.cc/T3jsZrnw/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup()
+{
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet= ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+       console.log(results);
+       noseX = results[0].pose.nose.x;
+       noseY = results[0].pose.nose.y;
+       console.log("nose x =" + noseX);
+       console.log("nose y =" + noseY);
+    } 
+}
+
+function draw()
+{
+image(video,0,0,300,300);
+image(clown_nose ,noseX,noseY ,30,30);
+}
+
+function take_snapshot()
+{
+save('mySelfie.png');
+}
